13bb4bcf09cfbf93d13f39780117cd4cd0d20087
Woody <2997336+MWGMorningwood@users.noreply.github.com>
2024-06-11 15:22:47 -0400

    Create WinFeatureShould-Be.psm1

---
+++ Function/WinFeatureShould-Be.psm1
@@ -0,0 +1,29 @@
+Param(
+    [Parameter(Mandatory=$True,HelpMessage='Specifies the name of the feature to be enabled. Feature names are case sensitive if you are servicing a Windows image running a version of Windows earlier than Windows® 8. You can use Get-WindowsOptionalFeature to find the name of the feature in the image.')]
+    [ValidateSet('Enabled','Disabled')]
+    [String]$State,
+    [Parameter(Mandatory=$True,HelpMessage='Specifies the state that the feature should be in. This function will set the feature to the correct state if it is not already.')]
+    [String]$Feature
+)
+
+switch ($method) {
+    Default {
+        Write-Host 'Set a method when you test.'
+    }
+    "test" {
+        $Current_Feature_State = Invoke-ImmyCommand { $feature = Get-WindowsOptionalFeature -FeatureName $using:Feature -Online; [string]$feature.State }
+        $Feature_Test = ($Current_Feature_State -eq $State)   
+        return $Feature_Test
+    }
+    "set" {
+        If ($State -eq 'Disabled') {
+            Invoke-ImmyCommand { $result = Disable-WindowsOptionalFeature -Online -FeatureName $using:Feature; return $result }
+            Write-Host "$Feature has been disabled."
+        } elseif ($State -eq 'Enabled') {
+            Invoke-ImmyCommand { $result = Enable-WindowsOptionalFeature -Online -FeatureName $using:Feature -all; return $result }
+            Write-Host "$Feature has been disabled."
+        } else {
+            Write-Host "$Feature has already been disabled."
+        }
+    }
+}
