9cca49056b0b25b844a3398a64dfb37bd29091b9
Woody <2997336+MWGMorningwood@users.noreply.github.com>
2024-07-10 15:51:14 -0400

    Create Set-DellBIOSProvider.ps1

---
+++ Function/Set-DellBIOSProvider.ps1
@@ -0,0 +1,51 @@
+#$BIOSPath = "PowerManagement\AutoOn"
+#$BIOSValue = "Everyday"
+#$method = "test"
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$Path,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Value
+)
+
+Ensure-PSModulePresent DellBiosProvider
+
+$BIOSPath = $Path
+$BIOSValue = $Value
+
+switch ($method) {
+    "test" {
+        Invoke-ImmyCommand {
+            Import-Module DellBIOSProvider
+            try{
+                $BIOSValue_Test = (Get-ChildItem -Path "DellSmbios:\$($using:BIOSPath)" -ErrorAction Stop).CurrentValue
+            } catch {
+                Write-Warning "Path failed to resolve. Please check script logs if this is unexpected."
+                Write-Verbose $BIOSValue_Test
+                return $false 
+            }
+
+            #Check actual setting value
+            if ($BIOSValue_Test -eq $using:BIOSValue){
+                Write-Host "Test results indicate success: $BIOSValue_Test"
+                return $true
+            } else {
+                Write-Warning "Test results do not indicate success: $BIOSValue_Test"
+                return $false
+            }
+        }
+        
+    }
+    "set" {
+        #Enable-DellSMBIOS
+        Invoke-ImmyCommand {
+            Import-Module DellBIOSProvider
+            Write-Host "Setting BIOS Value in DellSmbios:\$($using:BIOSPath)..."
+            Set-Item -Path "DellSmbios:\$($using:BIOSPath)" -value $using:BIOSValue
+        }
+    }
+    Default {
+        Write-Warning "No Method has been set. If you are running this outside of a maintenance session, uncomment the line that includes `$method"
+    }
+}
