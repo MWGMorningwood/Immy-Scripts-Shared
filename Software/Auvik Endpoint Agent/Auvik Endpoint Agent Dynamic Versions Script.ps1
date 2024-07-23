1ed941aa7574668333d849d1ada148228574473c
Woody <2997336+MWGMorningwood@users.noreply.github.com>
2024-07-23 10:58:53 -0400

    Create Auvik Endpoint Agent Dynamic Versions Script.ps1

---
+++ Software/Auvik Endpoint Agent/Auvik Endpoint Agent Dynamic Versions Script.ps1
@@ -0,0 +1,7 @@
+<#
+  Software: Auvik Endpoint Agent
+  Type: Dynamic Versioning
+  Context: Cloud
+#>
+
+Get-DynamicVersionFromInstallerURL "https://apt.my.auvik.com/endpoint/module-manager/latest/windows-amd64/AuvikAgent.msi"
