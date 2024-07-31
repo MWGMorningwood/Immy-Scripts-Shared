6c5bf9f54b1e21cd0a5d214a450a31913d26eecf
Woody <logan@mw-gc.com>
2024-07-31 18:47:56 -0400

    chore: Update Map Azure Drive Combined Script.ps1 to map Azure drives at logon

--- Task/Map Azure Drive Combined Script.ps1
+++ Task/Map Azure Drive Combined Script.ps1
@@ -1,10 +1,18 @@
 param(
-[Parameter(Position=0,Mandatory=$True,HelpMessage=@'
+[Parameter(Position=1,Mandatory=$True,HelpMessage=@'
+Enter the name of your Azure Storage account.
+'@)]
+[String]$storageAccount,
+[Parameter(Position=1,Mandatory=$True,HelpMessage=@'
+Enter your storage Access Key or password.
+'@)]
+[Password()]$accessKey,
+[Parameter(Position=2,Mandatory=$True,HelpMessage=@'
 Array of drives to map at logon. Format is as follows:  
-| Name | Value           |
-|------|-----------------|
-| `Q`  | `\\path\for\Q\` |
-| `T`  | `\\path\for\T\` |
+| Name         | Value |
+|--------------|-------|
+| `appshares`  | `Q`   |
+| `timesheets` | `T`   |
 '@)]
 [Hashtable]$driveArray
 )
@@ -13,22 +21,24 @@ Array of drives to map at logon. Format is as follows:
 #net use Q: $sharePath /persistent:yes <- old script
 
 $driveArray.GetEnumerator() | ForEach-Object {
-    $driveName = $_.Name
-    $driveValue = $_.Value
-    Write-Host "Creating task for mapping drive $driveName to $driveValue"
+    $shareName = $_.Name
+    $driveLetter = $_.Value
+    $storageUrl = "$storageAccount.file.core.windows.net"
+    $UNCpath = "\\$storageUrl\$shareName"
+    Write-Host "Creating task for mapping drive $driveLetter to $UNCpath"
 
     # Writing codeblock as string first to forcefully eval variables
     $scriptString = @"
 try {
     # Mount the drive
-    New-PSDrive -Persist -Name '$driveName' -PSProvider FileSystem -Root '$driveValue' -Scope Global
+    New-SMBMapping -LocalPath "$($driveLetter):" -RemotePath $UNCpath -UserName "localhost\$storageAccount" -Password $accessKey
 } catch {
-    Write-Error -Message "Unable to mount the drive. Check credentials and connectivity to '$driveValue'"
+    Write-Error -Message "Unable to mount the drive. Check credentials and connectivity to '$storageUrl'"
 }
 "@
     # Below two lines used to convert to codeblock and dodge PS Constrained mode in Immy metascript env.
     [scriptblock]$script = {}
     $script = Add-CodeToScriptBlock $script $scriptString
 
-    Set-ScheduledTaskBeta -TaskName "Map $driveName Drive" -TaskDesc "Map the $driveName drive at logon via New-PSDrive to $driveValue" -Trigger AtLogOn -ScriptContent $script
+    Set-ScheduledTask -TaskName "Map $driveLetter Drive" -TaskDesc "Map the $driveLetter drive at logon via New-PSDrive to $UNCpath" -Trigger AtLogOn -ScriptContent $script
 }
