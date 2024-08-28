bd6ad1a3bb567241dacf711b155f3d47c13e64e8
smood922 <48338396+smood922@users.noreply.github.com>
2024-08-27 17:35:52 -0700

    adding Inventory Local Administrators and AD Group Memberships Inventory with accompanying functions

---
+++ Function/Get-EntraIDObjectByGuidArray.ps1
@@ -0,0 +1,27 @@
+ <#
+.SYNOPSIS
+Gets associated Entra ID objects for an input list of GUIDs
+
+.DESCRIPTION
+Returns an array of objects corresponding to the input GUIDs from the Entra ID tenant
+
+.PARAMETER GuidArray
+An array of 1 or more GUIDs to look up
+#>
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [String[]]
+    $GuidArray
+)
+
+$Headers = Get-ImmyAzureAuthHeader
+$GraphURI = "https://graph.microsoft.com/beta/directoryObjects/getByIds"
+
+$Body = @{
+    ids = $GuidArray
+} | ConvertTo-Json
+
+$objects = Invoke-RestMethod -Uri $GraphURI -Headers $Headers -Method Post -ContentType "application/json" -Body $Body  | Select-Object -ExpandProperty Value
+
+return $objects
---
+++ Function/Get-ImmyADComputerGroups.ps1
@@ -0,0 +1,26 @@
+param(
+    [string]$ComputerName, 
+    [string]$DomainName,
+    [string]$PreferredDomainControllerName
+)
+
+$DomainController = Get-ImmyDomainController -DomainName $DomainToJoin -PreferredDomainControllerName $PreferredDomainControllerName
+$ExistingComputer = Get-ImmyADComputer @PSBoundParameters
+
+if ($ExistingComputer) {
+    Write-Host "Computer Found! Looking up Group Memberships for $($ExistingComputer.Name) Using Domain Controller $($DomainController.Name)"
+
+    $ExistingComputerGroups = Invoke-ImmyCommand -Computer $DomainController {
+        Import-Module ActiveDirectory -Verbose:$false
+        #Write-Host -Fore Green $env:computername
+        if($using:ComputerName) {
+            try {
+                Get-ADPrincipalGroupMembership (Get-ADComputer $using:ComputerName).DistinguishedName
+            }
+            catch {
+            }
+        }
+    }
+}
+
+$ExistingComputerGroups
