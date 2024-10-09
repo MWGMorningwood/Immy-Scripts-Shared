96edde322ab65588ffb0cf22d5bee2d9ee4d3bc8
Woody <2997336+MWGMorningwood@users.noreply.github.com>
2024-10-09 09:23:15 -0400

    Update Inventory Local Administrators Including Entra.ps1

--- Inventory/Inventory Local Administrators Including Entra.ps1
+++ Inventory/Inventory Local Administrators Including Entra.ps1
@@ -9,8 +9,8 @@
 $administrators = Invoke-Immycommand {
     $administrators = @(
             ([ADSI]"WinNT://./Administrators").psbase.Invoke('Members') |
-            ForEach-Object { 
-                $_.GetType().InvokeMember('AdsPath', 'GetProperty', $null, $($_), $null) 
+            ForEach-Object {
+                $_.GetType().InvokeMember('AdsPath', 'GetProperty', $null, $($_), $null)
             }
         ) -match '^WinNT';
     $administrators
@@ -18,7 +18,7 @@ $administrators = Invoke-Immycommand {
 
 $administrators = $administrators -replace 'WinNT://', ''
 
-# Get unresolved Entra-formatted SIDs and convert to GUIDs. 
+# Get unresolved Entra-formatted SIDs and convert to GUIDs.
 # See https://oliverkieselbach.com/2020/05/13/powershell-helpers-to-convert-azure-ad-object-ids-and-sids/
 $EntraGUIDTable = $administrators -match "S-1-12-1*" | ForEach-Object {
         $object = New-Object -TypeName PSObject
@@ -33,7 +33,7 @@ $EntraObjects = Get-EntraIDObjectByGuidArray -GuidArray $($EntraGUIDTable.guid)
 
 # For each entry, we'll replace with the friendly info if we have it, or mark as unknown
 foreach ($Entry in $EntraGUIDTable) {
-    $EntraObject = $EntraObjects | ? {$_.id -match $Entry.Guid }
+    $EntraObject = $EntraObjects | Where-Object {$_.id -match $Entry.Guid }
     if ($EntraObject) {
         $administrators = $administrators -replace $($Entry.Sid),"[$($EntraObject.'@odata.type')] $($EntraObject.displayName)"
     } else {
