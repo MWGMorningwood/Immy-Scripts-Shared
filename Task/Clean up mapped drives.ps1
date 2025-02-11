f9945b3bdda0853522b41bc3a0efc9fdf5c317ce
MWGMorningwood <2997336+MWGMorningwood@users.noreply.github.com>
2025-02-11 18:24:17 -0500

    FEAT: Add script to manage network drives with permitted list and reboot handling

---
+++ Task/Clean up mapped drives.ps1
@@ -0,0 +1,50 @@
+<#
+Author: Logan Cook
+Notes: Combined script with in-script parameters. This script will remove any network drives that are not in the list of permitted drives. This script will also set a pending reboot flag if any drives are removed.
+#>
+
+param (
+    [Parameter(Position=0,HelpMessage='Enter drive letters in the format of `R:`, then hit add.')]
+    [string[]]$PermittedDrives
+)
+
+$networkDrives = Invoke-ImmyCommand -Context "User" {
+    $result = Get-WmiObject -Query "SELECT * FROM Win32_NetworkConnection"
+    return $result
+}
+
+switch($method){
+    "test"{
+        Invoke-ImmyCommand -Context "User" {
+            $allPermitted = $true
+            foreach ($drive in $using:networkDrives) {
+                if ($using:PermittedDrives -notcontains $drive.LocalName) {
+                    $allPermitted = $false
+                    Write-Host "Non-permitted network drive found:" $drive.LocalName
+                } else {
+                    Write-Host "Permitted network drive:" $drive.LocalName
+                }
+            }
+            return $allPermitted
+        }
+    }
+    "set"{
+        Invoke-ImmyCommand -Context "User" {
+            foreach ($drive in $using:networkDrives) {
+                if ($using:PermittedDrives -notcontains $drive.LocalName) {
+                    # Remove the network drive
+                    Remove-PSDrive -Name $drive.LocalName.Substring(0, 1) -Force
+                    Write-Host "Removed non-permitted network drive:" $drive.LocalName
+                    $pendingReboot = $true
+                }
+            }
+            if ($pendingReboot -eq $true) {
+                Set-PendingRebootFlag
+            }
+            if($pendingReboot -and $RebootPreference -ne "Suppress") {
+                Write-Host "Restarting to clear mapped drive references."
+                Restart-ComputerAndWait
+            }
+        }
+    }
+}
